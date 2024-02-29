86b0110c5cf1b31633a3dc2f919796a12c2e685b
alessiarabacchi <alessia.rabacchi@icloud.com>
2024-02-29 14:21:39 +0100

    changes

--- todo.php
+++ todo.php
@@ -1,13 +1,23 @@
+
 <?php
 $todoFile = 'todos.json';
 
-header('Content-Type: application/json');
-
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+   
     echo file_get_contents($todoFile);
 } elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    
     $input = json_decode(file_get_contents('php://input'), true);
+    $newTodo = $input['todo'];
+
+    
     $todos = json_decode(file_get_contents($todoFile), true);
-    $todos[] = $input['todo'];
+    $todos[] = $newTodo;
+
+    
     file_put_contents($todoFile, json_encode($todos));
+
+    
+    echo json_encode(["success" => true]);
 }
+?>
